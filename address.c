fb8e1eef8075404c7ad614e697efd794957e889f
Cranberry1997 <1909425849@qq.com>
2019-11-05 14:54:24 +0800

    adds

---
+++ address.c
@@ -0,0 +1,45 @@
+
+请先确认主板型号（详细比对排母侧边贴纸上的编号）
+
+----------------------------------------------------------------------------------------------------
+Win10 Home系统
+S70CR110 z8300 2/32 
+链接：https://pan.baidu.com/s/1rt1rteV7MP5NdL89LZ2PPQ 密码：hu9b
+
+S70CR110 z8300 4/64
+链接：https://pan.baidu.com/s/1m6UCZger_C-o4JDai2Fodw 密码：brwk
+
+S70CR110 z8350 2/32
+链接：https://pan.baidu.com/s/1q44inrYMI1nwR2FLGo1-BA 密码：8btc
+
+S70CR110 z8350 4/64
+链接：https://pan.baidu.com/s/187P3PxJDkNjOBNFQFXnG6Q 密码：4lxx
+
+S70CR200 z8350 2/32 & 4/64 （DFR0444 DFR0418 DFR0419）
+链接：https://pan.baidu.com/s/1UVVBgh4MpdvlpR70YmDu7g 密码：eorq
+
+S70GR200 N4100 4/64 （DFR0543 DFR0544）
+链接：https://pan.baidu.com/s/1-5qCue5K-L3YRcvv5phO7A 密码：bbw0 
+
+S70KR200 M3-7Y30 (旧版本DFR0545 DFR0546 DFR0547)
+链接：https://pan.baidu.com/s/1z2fOqtqtquJDiKJOC0nUWQ 密码：h4t6
+
+S70KR300 M3-8100Y (新版本DFR0545 DFR0546 DFR0547)
+链接：https://pan.baidu.com/s/1tvNVd377ENmPqzOcxJ9wTQ 密码：e621
+
+
+----------------------------------------------------------------------------------------------------
+
+Win10 IOT Enterprise 系统 （DFR0470-ENT）
+S70CR200 z8350 2/32 & 4/64 
+链接：https://pan.baidu.com/s/1gtasNxe9h4oXvn_jlLA1Sw 密码：26md
+
+----------------------------------------------------------------------------------------------------
+最新版本bios 
+
+请先确认主板型号（详细比对排母侧边贴纸上的编号）
+https://github.com/LattePandaTeam/LattePanda-Win10-Software/tree/master/Bios
+
+----------------------------------------------------------------------------------------------------
+驱动包
+https://github.com/LattePandaTeam/LattePanda-Win10-Software/tree/master/Drivers
